89da055294db7e76945f2b8a643847a942e85a20
KrystynaCal <KrystynaCal5@gmail.com>
2022-11-21 22:44:25 +0100

    Add hotel search without hotel/city name

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -45,4 +45,29 @@ public class HotelSearch {
         Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3));
 
     }
+
+    @Test
+    public void searchHotelWithoutName() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.name("checkin")).sendKeys("20/11/2022");
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+        WebElement noResultHeading = driver.findElement(By.xpath("//div[@class='itemscontainer']//h2"));
+
+        Assert.assertTrue(noResultHeading.isDisplayed());
+        Assert.assertEquals(noResultHeading.getText(), "No Results Found");
+    }
 }
+
+
