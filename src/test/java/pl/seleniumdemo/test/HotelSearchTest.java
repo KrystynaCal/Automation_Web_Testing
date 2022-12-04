e98b0181c72b761ac6e25799b68f63236dbff8c9
KrystynaCal <KrystynaCal5@gmail.com>
2022-12-04 23:51:31 +0100

    Add ResultsPage

--- src/test/java/pl/seleniumdemo/test/HotelSearchTest.java
+++ src/test/java/pl/seleniumdemo/test/HotelSearchTest.java
@@ -1,28 +1,26 @@
 package pl.seleniumdemo.test;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.seleniumdemo.pages.HotelSearchPage;
+import pl.seleniumdemo.pages.ResultsPage;
+
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class HotelSearchTest extends BaseTest {
 
     @Test
     public void searchHotelTest() {
+
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
         hotelSearchPage.setCity("Dubai");
         hotelSearchPage.setDates("27/04/2021", "29/04/2021");
-        hotelSearchPage.setTravellers();
+        hotelSearchPage.setTravellers(1,1);
         hotelSearchPage.performSearch();
 
-        List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class, 'list_title')]//b")).stream()
-                .map(e1 -> e1.getAttribute("textContent"))
-                .collect(Collectors.toList());
+        ResultsPage resultsPage = new ResultsPage(driver);
+        List<String> hotelNames = resultsPage.getHotelNames();
 
-        hotelNames.forEach(System.out::println);
 
         Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0));
         Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1));
@@ -33,20 +31,13 @@ public class HotelSearchTest extends BaseTest {
 
     @Test
     public void searchHotelWithoutNameTest() {
-        driver.findElement(By.name("checkin")).sendKeys("20/11/2022");
-        driver.findElement(By.name("checkout")).click();
-        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
-                .stream()
-                .filter(WebElement::isDisplayed)
-                .findFirst()
-                .ifPresent(WebElement::click);
-        driver.findElement(By.id("travellersInput")).click();
-        driver.findElement(By.id("childPlusBtn")).click();
-        driver.findElement(By.xpath("//button[text()=' Search']")).click();
-        WebElement noResultHeading = driver.findElement(By.xpath("//div[@class='itemscontainer']//h2"));
-
-        Assert.assertTrue(noResultHeading.isDisplayed());
-        Assert.assertEquals(noResultHeading.getText(), "No Results Found");
+        HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
+        hotelSearchPage.setDates("20/11/2022","30/11/2022");
+        hotelSearchPage.setTravellers(0,1);
+        hotelSearchPage.performSearch();
+        ResultsPage resultsPage = new ResultsPage(driver);
+        Assert.assertTrue(resultsPage.resultHeading.isDisplayed());
+        Assert.assertEquals(resultsPage.getHeadingText(), "No Results Found");
     }
 }
 
