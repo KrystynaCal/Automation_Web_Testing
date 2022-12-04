6a9126eeee25897aa7680d20590f67b3c034f9dc
KrystynaCal <KrystynaCal5@gmail.com>
2022-12-04 20:53:34 +0100

    Adding first page object

--- src/test/java/pl/seleniumdemo/test/BaseTest.java
+++ src/test/java/pl/seleniumdemo/test/BaseTest.java
@@ -1,9 +1,10 @@
+package pl.seleniumdemo.test;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import java.time.Duration;
 
 public class BaseTest {
